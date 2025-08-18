d3dfba614831a3a43bfa5ba292eefb811da079f3
BarbaraStrbikova <barbara.strbikova@seznam.cz>
2025-08-18 13:24:07 +0200

    uprava_header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,7 @@ export function Header() {
   return (
        <header className="header container">
         <h1 className="header__title">Moje domacnost</h1>
-        <nav className="header__nav">
-            <a className="header__menu" href="#">Home</a>
-            <a className="header__menu" href="#">Úkoly</a>
-            <a className="header__menu" href="#">Zasoby</a>
-        </nav>
+        
 
     </header>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.jsx'
 
-import { BrowserRouter,
-        Routes,
-        Route,
- } from 'react-router'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
